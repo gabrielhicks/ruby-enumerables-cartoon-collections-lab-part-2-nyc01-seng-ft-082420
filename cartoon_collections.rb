e457f1170f933b801f12bf107cdecf1c39510aaf
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 14:55:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
   planeteer_calls.find do |string|
-    string[valid_calls]
+    valid_calls
   end
   
 end
